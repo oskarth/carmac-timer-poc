d916f8cfde26ab8cdd97cedd40f98ff39b37f236
Oskar Thoren <ot@oskarthoren.com>
2020-05-30 14:39:44 +0800

    Basic timer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+
+// TODO Redraw timer, don't append
+// TODO Time in minutes
+// TODO Pause functionality
+
 fn main() {
-    println!("Hello, world!");
+    let now = Instant::now();
+    println!("Starting a timer");
+
+    loop {
+        sleep(Duration::new(1, 0));
+        println!("{}", now.elapsed().as_secs());
+    }
 }
